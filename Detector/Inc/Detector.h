bbf4c09856cbaa0dc915e60d97d90d8705345822
7emotions <paradise_c@qq.com>
2023-10-28 21:45:05 +0800

    add hikvision

--- Detector/Inc/Detector.h
+++ Detector/Inc/Detector.h
@@ -11,6 +11,14 @@
 #include "LightDescriptor.h"
 
 // TODO: 在此处引用程序需要的其他标头。
+using namespace cv;
+using namespace std;
+
+const int minArea = 5;
+const int minContoursSize = 1;
+const double lightLengthRatio = 4.0;
+const double minAngleDiff = 10.0;
+const double minLengthDiffRatio = 0.8;
 
 class Detector {
 public:
@@ -20,6 +28,7 @@ public:
 	~Detector();
 
 	cv::Mat preprocess(cv::Mat img, COLOR_TAG tag);
+	Mat fetchLights(cv::Mat img, COLOR_TAG tag);
 
 private:
 };
@@ -34,38 +43,105 @@ inline cv::Mat Detector::preprocess(cv::Mat img, COLOR_TAG tagToDetect) {
 	cv::Mat binary, gaussian, dilate;
 	cv::split(img, channels);
 	cv::threshold(channels[tagToDetect == BLUE ? 0 : 2], binary, 220, 255, 0);
+	std::cout<<binary.size()<<std::endl;
+	GaussianBlur(binary, gaussian, cv::Size(5, 5), 0);
 
 	auto element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
-
-	cv::GaussianBlur(binary, gaussian, cv::Size(5, 5), 0);
 	cv::dilate(gaussian, dilate, element);
-
-	//cv::imshow("pro", dilate);
 
 	return dilate;
 
-	//cv::Mat hsv;
-	//cv::cvtColor(img, hsv, cv::COLOR_BGR2HSV);
-	//cv::Mat mask;
-
-	//if (tagToDetect == COLOR_TAG::BLUE) {
-	//	cv::inRange(hsv, cv::Scalar(100, 43, 46), cv::Scalar(124, 255, 255), mask);
-
-	//}
-	//else if (tagToDetect == COLOR_TAG::RED) {
-	//	cv::inRange(hsv, cv::Scalar(0, 43, 46), cv::Scalar(10, 255, 255), mask);
-	//	cv::Mat mask1;
-	//	cv::inRange(hsv, cv::Scalar(156, 43, 46), cv::Scalar(180, 255, 255), mask1);
-
-	//	cv::bitwise_and(img, img, img, mask1);
-	//}
-
-	//cv::bitwise_and(img, img, img, mask);
-	//cv::Mat bin;
-	//cv::cvtColor(img, bin, cv::COLOR_RGB2GRAY);
-	//cv::threshold(bin, bin, 100, 255, cv::THRESH_BINARY);
-
-	//cv::imshow("bin", bin);
-
-	//return bin;
 }
+
+Mat Detector::fetchLights(cv::Mat img, COLOR_TAG color_tag) {
+	Detector myDetector;
+	vector<LightDescriptor> lights;
+
+	std::vector<std::vector<cv::Point>> contours;
+	std::vector<cv::Vec4i> hierarchy;
+
+
+	cv::findContours(myDetector.preprocess(img, color_tag), contours,
+		hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_NONE,
+		cv::Point());
+
+	std::vector<cv::RotatedRect> boundRect(contours.size());
+	cv::Point2f rect[4];
+
+	for (int i = 0; i < contours.size(); i++) {
+		double area = contourArea(contours[i]);
+		if (area < minArea || contours[i].size() <= minContoursSize)
+		{
+			continue;
+		}
+
+		RotatedRect rec = fitEllipse(contours[i]);
+
+		if (rec.size.width / rec.size.height > lightLengthRatio)
+		{
+			continue;
+		}
+
+		rec.size.height *= 1.2;
+		rec.size.width *= 1.2;
+
+		lights.push_back(rec);
+	}
+
+	sort(lights.begin(), lights.end(), [&](const LightDescriptor& a, const LightDescriptor& b) {
+		return a.angle == b.angle ? a.angle < b.angle : a.angle > b.angle;
+		});
+
+	for (int i = 0; i < lights.size(); i++)
+	{
+		int j = i + 1;
+		for (; j < lights.size(); j++)
+		{
+			if (abs(lights[i].angle - lights[j].angle) > minAngleDiff)
+			{
+				break;
+			}
+		}
+
+		for(;j>=0;j--){
+			auto ml = (lights[i].center.x - lights[j].center.x) / 2;
+			auto rpd = abs(lights[i].length-lights[j].length)/max(lights[i].length,lights[j].length);
+			if(rpd > 0.8){
+				continue;
+			}
+			auto rdfm = abs(lights[i].length - ml) / ml;
+			if (rdfm > 0.5){
+				continue;
+			}
+			auto distance = 
+				sqrt(pow(lights[i].center.x - lights[j].center.x, 2) + pow(lights[i].center.y - lights[j].center.y, 2));
+			auto rdsm = distance / ml;
+			if (rdsm > 3.5 || rdsm < 0.5){
+				continue;
+			}
+			auto dx = abs(lights[i].center.x - lights[j].center.x);
+			auto dy = abs(lights[i].center.y - lights[j].center.y);
+			auto dxr = dx / ml;
+			auto dyr = dy / ml;
+			if(dxr > 2 || dxr<0.6){
+				continue;
+			}
+			if(dyr>1.2){
+				continue;
+			}
+
+			Point2f center((lights[i].center.x+lights[j].center.x)/2,
+					(lights[i].center.y+lights[j].center.y)/2);
+			auto angle = (lights[i].angle + lights[j].angle)/2;
+			RotatedRect rect(center, Size(distance, ml), angle);
+			vector<Point2f> points;
+			rect.points(points);
+
+			for(int k=0;k<points.size();k++){
+				line(img, points[k], points[(k+1)%points.size()], Scalar(0, 0, 255), 2);
+			}
+		}
+	}
+	return img;
+}
+
--- Detector/src/Detector.cpp
+++ Detector/src/Detector.cpp
@@ -5,163 +5,29 @@
 
 #define DEBUG
 
-const int minArea = 5;
-const int minContoursSize = 1;
-const double lightLengthRatio = 4.0;
-const double minAngleDiff = 10.0;
-const double minLengthDiffRatio = 0.8;
 
 using namespace cv;
 using namespace std;
 
 std::string dir =
-R"(D:\RMWorks\images\red\processed)";
-
-void test(cv::Mat img, Detector::COLOR_TAG color_tag) {
-	img.resize(img.rows, img.cols);
-
-	Detector myDetector;
-	vector<LightDescriptor> lights;
-
-	std::vector<std::vector<cv::Point>> contours;
-	std::vector<cv::Vec4i> hierarchy;
-
-
-	cv::findContours(myDetector.preprocess(img, color_tag), contours,
-		hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_NONE,
-		cv::Point());
-
-	std::vector<cv::RotatedRect> boundRect(contours.size());
-	cv::Point2f rect[4];
-
-	for (int i = 0; i < contours.size(); i++) {
-		double area = contourArea(contours[i]);
-		if (area < minArea || contours[i].size() <= minContoursSize)
-		{
-			continue;
-		}
-
-		RotatedRect rec = fitEllipse(contours[i]);
-
-		if (rec.size.width / rec.size.height > lightLengthRatio)
-		{
-			continue;
-		}
-
-		rec.size.height *= 1.2;
-		rec.size.width *= 1.2;
-
-		lights.push_back(rec);
-#pragma region pre
-		//RotatedRect rec = fitEllipse(contours[i]);
-
-
-		//boundRect[i] = cv::minAreaRect(cv::Mat(contours[i]));
-		//boundRect[i].points(rect);
-
-		//for (int j = 0; j < 4; j++) {
-		//	cv::line(img, rect[j], rect[(j + 1) % 4],
-		//		color_tag == Detector::COLOR_TAG::BLUE ?
-		//		cv::Scalar(0, 0, 255) :
-		//		cv::Scalar(255, 0, 0), 2, 8);
-		//}
-#pragma endregion
-	}
-
-	sort(lights.begin(), lights.end(), [&](const LightDescriptor& a, const LightDescriptor& b) {
-		return a.angle == b.angle ? a.angle < b.angle : a.angle > b.angle;
-		});
-
-	for (int i = 0; i < lights.size(); i++)
-	{
-		int j = i + 1;
-		for (; j < lights.size(); j++)
-		{
-			if (abs(lights[i].angle - lights[j].angle) > 10)
-			{
-				break;
-			}
-		}
-
-	}
-}
+R"(/home/alliance/gitrepo/UGAS-beta/Detector/resources/)";
 
 int main() {
-	
+
 	std::vector<std::string> files;
+	Detector detector;
 
 	cv::glob(dir, files);
 	for (auto i = 0; i < files.size(); i++)
 	{
-#pragma region label
-		//try
-		//{
-		//	tinyxml2::XMLDocument doc;
-		//	doc.LoadFile(files[i].c_str());
-		//	auto annotation = doc.RootElement();
-		//	auto filename = annotation->FirstChildElement("filename");
-		//	if (!filename)
-		//	{
-		//		break;
-		//	}
-		//	std::cout << filename->GetText() << std::endl;
-
-		//	auto img = cv::imread(dir + "image\\" + filename->GetText());
-
-		//	auto object = annotation->FirstChildElement("object");
-
-		//	while (object)
-		//	{
-		//		auto color = object->FirstChildElement("armor_color");
-		//		if (!color)
-		//		{
-		//			object = object->NextSiblingElement();
-		//			continue;
-		//		}
-		//		auto bndbox = object->FirstChildElement("bndbox");
-		//		auto xmin = atoi(bndbox->FirstChildElement("xmin")->GetText());
-		//		auto xmax = atoi(bndbox->FirstChildElement("xmax")->GetText());
-		//		auto ymin = atoi(bndbox->FirstChildElement("ymin")->GetText());
-		//		auto ymax = atoi(bndbox->FirstChildElement("ymax")->GetText());
-		//		
-		//		test(img(cv::Range(ymin, ymax), cv::Range(xmin, xmax)),
-		//			strcmp(color->GetText(), "blue") == 0 ?
-		//			Detector::BLUE :
-		//			Detector::RED);
-
-		//		object = object->NextSiblingElement();
-		//	}
-
-		//	//test(img);
-		//}
-		//catch (const std::exception& e)
-		//{
-		//	std::cout << e.what() << std::endl;
-		//}
-#pragma endregion
-		Mat img = imread(files[i]);
-
-		test(img, Detector::BLUE);
-		waitKey();
+		std::cout<<files[i]<<std::endl;
+		Mat img = imread(files[i]);	
+		img = detector.fetchLights(img,Detector::COLOR_TAG::RED);
+		imshow("image", img);
+		waitKey(0);
 	}
 
 	cv::destroyAllWindows();
 
-
-#if false
-
-	bool hash[90] = { false };
-
-	for (int i = 0; i < boundRect.size(); i++) {
-		if (hash[(int)boundRect[i].angle]) {
-
-		}
-		else {
-			hash[(int)boundRect[i].angle] = true;
-	}
-}
-
-
-#endif
 	return 0;
 }

0400bccc57ecde71bc8eb463fa7096ae7fda7bd0
7emotions <paradise_c@qq.com>
2023-10-29 18:55:37 +0800

    adjust structure

--- Detector/src/LightDescriptor.cpp
+++
@@ -1,2 +0,0 @@
-#include "../Inc/LightDescriptor.h"
-
---
+++ src/Detector/Detector.cpp
@@ -0,0 +1,39 @@
+﻿#include "Detector.h"
+
+#include <iostream>
+#include <algorithm>
+
+#include <opencv2/opencv.hpp>
+
+
+
+// #include "Detector/LightDescriptor.h"
+
+// #define DEBUG
+
+
+// using namespace cv;
+// using namespace std;
+
+// std::string dir =
+// R"(/home/alliance/gitrepo/UGAS-beta/Detector/resources/)";
+
+int main() {
+
+	// std::vector<std::string> files;
+	// Detector detector;
+
+	// cv::glob(dir, files);
+	// for (auto i = 0; i < files.size(); i++)
+	// {
+	// 	std::cout<<files[i]<<std::endl;
+	// 	Mat img = imread(files[i]);	
+	// 	img = detector.fetchLights(img,Detector::COLOR_TAG::RED);
+	// 	imshow("image", img);
+	// 	waitKey(0);
+	// }
+
+	// cv::destroyAllWindows();
+
+	return 0;
+}

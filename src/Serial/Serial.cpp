b92bc911535f7b0867dbd5654fa915d6580a8f0e
7emotions <paradise_c@qq.com>
2023-11-21 22:14:47 +0800

    serial and crc

--- src/Serial/Serial.cpp
+++ src/Serial/Serial.cpp
@@ -1,6 +1,19 @@
 #include  "Serial/Serial.h"
+#include <unistd.h>
+
 
 void SerialUtil::send(){
+		clear();
 	
+	*pkg = pkg_head;
+	
+	for(size_t i=1;i<=pkg_size;i++){
+		*(pkg+i)=*(ptr+i-1);
+	}
+
+	*(pkg+pkg_size+1) = crc();
+	
+
+	write(serial, pkg, pkg_size);
 }
 
